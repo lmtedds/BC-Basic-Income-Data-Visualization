5852190208744ab9445dbb951aba29fb104e5eb5
Peter Hunnisett <peter@softwarebalm.com>
2019-09-02 16:50:42 -0600

    Fix marking active menu item - replace radiand specific code for menu handling to fix marking active   menu.

--- src/router/main_helper.ts
+++ src/router/main_helper.ts
@@ -8,12 +8,7 @@ app.on(ROUTER_EVENT, (route) => {
 	const menus = document.querySelectorAll(".navbar-nav li");
 	menus.forEach((menu) => { menu.classList.remove("active"); });
 
-	// Add active back to the active menu. For simplicity, routes
-	// start with "/" then have the major header (e.g. fertilizer or food), a "-",
-	// and then the minor header. Just look for the "#majorheader" portion of things
-	// for the drop down menu's anchor.
-	const index: number = route.indexOf("-");
-	const major: string = (index > 0 ? route.slice(0, index) : route).replace("/", "#");
-	const item = document.querySelector(`[href="${major}"]`);
+	// Mark the active route element with the active class
+	const item = document.querySelector(`[href='${route}']`);
 	item && item.parentElement.classList.add("active");
 });
