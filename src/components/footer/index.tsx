cf42dd9c77041f30c706711438afdd50c63eec44
Peter Hunnisett <peter@softwarebalm.com>
2019-09-12 07:02:57 -0600

    Add license information - Update copyright notice - Make repository MIT licensed and include license

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -2,11 +2,14 @@ import { app } from "apprun";
 
 import "./style.scss";
 
+export const THIS_LICENSE_URL = "/LICENSE.txt";
+export const THIRD_PARTY_LICENSES_URL = "/licenses.txt";
+
 const Footer = () => (
 		<>
 			<footer className="bg-website">
 				<div className="container">
-					<p className="m-0 text-center">Copyright &copy; Lindsay Tedds 2019</p>
+					<p className="m-0 text-center">Copyright &copy; 2019 Lindsay Tedds and Gillian Petit - <a href={THIS_LICENSE_URL}>Some Rights Reserved</a></p>
 				</div>
 			</footer>
 		</>
