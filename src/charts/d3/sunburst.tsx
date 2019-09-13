0ebffb4a78b347fff21c91a1b6ccb420c344b864
Peter Hunnisett <peter@softwarebalm.com>
2019-09-12 19:52:10 -0600

    Add sunburst chart - Add sunburst chart that's a little bit prototype.

---
+++ src/charts/d3/sunburst.tsx
@@ -0,0 +1,127 @@
+import * as d3 from "d3";
+
+// Based on https://observablehq.com/@d3/zoomable-sunburst
+export function buildZoomableSunburstChart(hierarchicalData, svgEle?: SVGElement) {
+	// Create a new svg node or use an existing one.
+	const svg = svgEle ? d3.select(svgEle) : d3.create("svg");
+
+	const width = 1000;
+	const radius = width / 6;
+
+	const arc = d3.arc()
+		.startAngle((d: any) => d.x0) // FIXME: type
+		.endAngle((d: any) => d.x1) // FIXME: type
+		.padAngle((d: any) => Math.min((d.x1 - d.x0) / 2, 0.005)) // FIXME: type
+		.padRadius(radius * 1.5)
+		.innerRadius((d: any) => d.y0 * radius) // FIXME: type
+		.outerRadius((d: any) => Math.max(d.y0 * radius, d.y1 * radius - 1)); // FIXME: type
+
+	const colour = d3.scaleOrdinal(d3.quantize(d3.interpolateRainbow, hierarchicalData.children.length + 1));
+
+	const partition = (data) => {
+		const rooted = d3.hierarchy(data)
+			.sum((d) => d.value)
+			.sort((a, b) => b.value - a.value);
+
+		return d3.partition()
+			.size([2 * Math.PI, rooted.height + 1])
+			(rooted);
+	};
+
+	const root = partition(hierarchicalData);
+
+	// Setup properties required for sunburst
+	root.each((d: any) => {
+		d.current = d;
+		d.data.name = d.data.program || d.data.admin || d.data.ministry; // FIXME: This should be in calling function
+	}); // FIXME: Type
+
+	svg
+		.attr("viewBox", `0 0 ${width} ${width}`)
+		.attr("perserveAspectRatio", "xMinYMin meet")
+		.style("font", "10px sans-serif");
+
+	const g = svg.append("g")
+		.attr("transform", `translate(${width / 2},${width / 2})`);
+
+	const path = g.append("g")
+		.selectAll("path")
+		.data(root.descendants().slice(1))
+		.join("path")
+			.attr("fill", (d: any) => { while (d.depth > 1) d = d.parent; return colour(d.data.name); }) // FIXME: Type
+			.attr("fill-opacity", (d: any) => arcVisible(d.current) ? (d.children ? 0.6 : 0.4) : 0) // FIXME: Type
+			.attr("d", (d: any) => arc(d.current)); // FIXME: Type
+
+	path.filter((d: any) => d.children) // FIXME: Type
+		.style("cursor", "pointer")
+		.on("click", clicked);
+
+	const format = d3.format(",d");
+	path.append("title")
+		.text((d: any) => `${d.ancestors().map((d2) => d2.data.name).reverse().join("/")}\n${format(d.value)}`); // FIXME: Type
+
+	const label = g.append("g")
+		.attr("pointer-events", "none")
+		.attr("text-anchor", "middle")
+		.style("user-select", "none")
+		.selectAll("text")
+		.data(root.descendants().slice(1))
+		.join("text")
+			.attr("dy", "0.35em")
+			.attr("fill-opacity", (d: any) => +labelVisible(d.current)) // FIXME: Type
+			.attr("transform", (d: any) => labelTransform(d.current)) // FIXME: Type
+			.text((d: any) => d.data.name); // FIXME: Type
+
+	const parent = g.append("circle")
+			.datum(root)
+			.attr("r", radius)
+			.attr("fill", "none")
+			.attr("pointer-events", "all")
+			.on("click", clicked);
+
+	function clicked(p) {
+		parent.datum(p.parent || root);
+
+		root.each((d: any) => d.target = { // FIXME: Type
+			x0: Math.max(0, Math.min(1, (d.x0 - p.x0) / (p.x1 - p.x0))) * 2 * Math.PI,
+			x1: Math.max(0, Math.min(1, (d.x1 - p.x0) / (p.x1 - p.x0))) * 2 * Math.PI,
+			y0: Math.max(0, d.y0 - p.depth),
+			y1: Math.max(0, d.y1 - p.depth),
+		});
+
+		const t = g.transition().duration(750);
+
+		// Transition the data on all arcs, even the ones that aren’t visible,
+		// so that if this transition is interrupted, entering arcs will start
+		// the next transition from the desired position.
+		path.transition(t)
+			.tween("data", (d: any) => {
+				const i = d3.interpolate(d.current, d.target);
+				return (t2) => d.current = i(t2);
+			})
+			.filter(function(d: any) { return +this.getAttribute("fill-opacity") || arcVisible(d.target); } as any) // FIXME: Type
+			.attr("fill-opacity", (d: any) => arcVisible(d.target) ? (d.children ? 0.6 : 0.4) : 0) // FIXME: Type
+			.attrTween("d", (d: any) => () => arc(d.current)); // FIXME: Type
+
+		label.filter(function(d: any) { return +this.getAttribute("fill-opacity") || labelVisible(d.target); } as any) // FIXME: Type
+			.transition(t)
+			.attr("fill-opacity", (d: any) => +labelVisible(d.target)) // FIXME: Type
+			.attrTween("transform", (d: any) => () => labelTransform(d.current)); // FIXME: Type
+	}
+
+	function arcVisible(d) {
+		return d.y1 <= 3 && d.y0 >= 1 && d.x1 > d.x0;
+	}
+
+	function labelVisible(d) {
+		return d.y1 <= 3 && d.y0 >= 1 && (d.y1 - d.y0) * (d.x1 - d.x0) > 0.03;
+	}
+
+	function labelTransform(d) {
+		const x = (d.x0 + d.x1) / 2 * 180 / Math.PI;
+		const y = (d.y0 + d.y1) / 2 * radius;
+		return `rotate(${x - 90}) translate(${y},0) rotate(${x < 180 ? 0 : 180})`;
+	}
+
+	return svg.node();
+}
