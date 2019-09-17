1872b9f8dd7912aef7692e3f7bffae7d5643d04c
Peter Hunnisett <peter@softwarebalm.com>
2019-09-17 09:46:29 -0600

    Add optional axes to force matrix graphs - Add independently optional x and y axes. - Add commented start for grid lines.

--- src/charts/d3/force_matrix.tsx
+++ src/charts/d3/force_matrix.tsx
@@ -99,8 +99,8 @@ export function buildMatrixForceChart(chartData: IMatrixChart, svgEle?: SVGEleme
 	// Figure out how to divide up the space
 	const width = 1000;
 	const height = 1000;
-	const yAxisWidth = dimensions.yAxis ? 200 : 0;
-	const xAxisHeight = dimensions.xAxis ? 200 : 0;
+	const yAxisWidth = dimensions.yAxis ? 100 : 0;
+	const xAxisHeight = dimensions.xAxis ? 100 : 0;
 	const colSpace = (width - yAxisWidth) / dimensions.cols;
 	const rowSpace = (height - xAxisHeight) / dimensions.rows;
 
@@ -119,10 +119,98 @@ export function buildMatrixForceChart(chartData: IMatrixChart, svgEle?: SVGEleme
 			// Find the middle of the appropriate row of the matrix to centre this datum.
 			return (Math.ceil((d.quad + 1) / dimensions.cols) - 0.5) * rowSpace;
 		}))
-		.force("collision", d3.forceCollide().radius(function(d: any) {
+		.force("collision", d3.forceCollide().radius(function(d: IMatrixDatum) {
 			return d.radius;
 		}))
 		.on("tick", ticked);    // FIXME: Verify the tick is unregistered when finished
+
+	// Generate locations for the ticks with names and the start and end ticks.
+	function middleFoo(start, end, num) {
+		const ticks = [start];
+		const stride = (end - start) / num;
+
+		for(let i = 0, at = start + stride * 0.5; i < num; ++i, at += stride) {
+			ticks.push(at);
+		}
+
+		ticks.push(end);
+
+		return ticks;
+	}
+
+	// Optionally add axes.
+	if(dimensions.xAxis) {
+		// Add empty first and last tick names that will be located at start and end of grids then
+		// setup a mapping to each tick name to it's location along the axis.
+		const xAxisNames = dimensions.xAxis ? [""].concat(chartData.axes.xTitles).concat("") : ["", ""];
+		const xOrdinalAxis = d3.scaleOrdinal()
+			.domain(xAxisNames)
+			.range(middleFoo(yAxisWidth, width, chartData.axes.xTitles.length));
+
+		svg.append("g")
+			.attr("class", "x-axis")
+			.attr("transform", `translate(0, ${height - xAxisHeight})`)
+			.call(d3.axisBottom(xOrdinalAxis as any)
+				.ticks(xAxisNames.length)
+				.tickSize(0),
+			)
+			.call((g) => g.select(".domain").remove()); // Get rid of the domain path for the axis
+		}
+
+	if(dimensions.yAxis) {
+		// Add empty first and last tick names that will be located at start and end of grids then
+		// setup a mapping to each tick name to it's location along the axis.
+		const yAxisNames = dimensions.yAxis ? [""].concat(chartData.axes.yTitles).concat("") : ["", ""];
+		const yOrdinalAxis = d3.scaleOrdinal()
+			.domain(yAxisNames)
+			.range(middleFoo(xAxisHeight, height, chartData.axes.yTitles.length));
+
+		svg.append("g")
+			.attr("class", "y-axis")
+			.attr("transform", `translate(${yAxisWidth}, ${-xAxisHeight})`)
+			.call(d3.axisLeft(yOrdinalAxis as any)
+				.ticks(yAxisNames.length)
+				.tickSize(0),
+			)
+			.call((g) => g.select(".domain").remove()); // Get rid of the domain path for the axis
+	}
+
+	// function edgeFoo(start, end, num) {
+	// 	let ticks = [start];
+	// 	const stride = (end - start) / num;
+
+	// 	for(let i = 0, at = start + stride; i < num; ++i, at += stride) {
+	// 		ticks.push(at);
+	// 	}
+
+	// 	return ticks;
+	// }
+
+	// const vertGridLines = [...Array(dimensions.cols + 1).keys()];
+	// const vertGridScale = d3.scaleOrdinal()
+	// 	.domain(vertGridLines as any)
+	// 	.range(edgeFoo(yAxisWidth, width, dimensions.cols));
+	// const vertGridlines = d3.axisTop(vertGridScale as any)
+	// 	.tickFormat("" as any) // FIXME: typing
+	// 	.tickSize(-height);
+
+	// const horGridLines = [...Array(dimensions.rows + 1).keys()];
+	// const horGridScale = d3.scaleOrdinal()
+	// 	.domain(horGridLines as any)
+	// 	.range(edgeFoo(0, height - xAxisHeight, dimensions.rows));
+	// const horGridlines = d3.axisLeft(horGridScale as any)
+	// 	.tickFormat("" as any) // FIXME: typing
+	// 	.tickSize(-width);
+
+	// svg.append("g")
+	// 	.attr("class", "vertgridlines")
+	// 	.style("stroke-dasharray", ("3,3"))
+	// 	.call(vertGridlines);
+
+	// svg.append("g")
+	// 	.attr("class", "horgridlines")
+	// 	.style("stroke-dasharray", ("3,3"))
+	// 	.call(horGridlines as any);
 
 	function ticked() {
 		const u = svg
