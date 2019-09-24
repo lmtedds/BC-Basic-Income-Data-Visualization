f2f4d7c15f9f27da1faf327dd0c78eb43a7fb192
Peter Hunnisett <peter@softwarebalm.com>
2019-09-24 10:49:02 -0600

    Build charts through AppRun view method - create svgs in component - update AppRun to 1.21.9 to get ability to pass elements to VDOM

--- src/pages/research/charts/index.tsx
+++ src/pages/research/charts/index.tsx
@@ -8,11 +8,6 @@ import "./style.scss";
 
 export const CHARTS_PAGE_URL: string = "/research-charts";
 
-const matrixChartId: string = "matrixChart";
-const linkChartId: string = "linkChart";
-const ministryCircleChartId: string = "ministryCircleChart";
-const ministrySunburstChartId: string = "ministrySunburstChart";
-
 export default class ChartsComponent extends Component {
 	public state = {};
 
@@ -20,35 +15,43 @@ export default class ChartsComponent extends Component {
 		[CHARTS_PAGE_URL]: (state) => state,
 	};
 
+	private readonly matrixSvgEle: SVGElement;
+	private readonly linkSvgEle: SVGElement;
+	private readonly circleSvgEle: SVGElement;
+	private readonly sunburstSvgEle: SVGElement;
+
+	constructor() {
+		super();
+
+		this.matrixSvgEle = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+		this.linkSvgEle = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+		this.circleSvgEle = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+		this.sunburstSvgEle = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+	}
+
 	public view = (state) => {
+		const matrixChart = buildInKindChart(this.matrixSvgEle); // Yes, this really is an SVG element
+		console.log(matrixChart);
+
+		const linkChart = buildProgramInteractionChart(this.linkSvgEle);
+		console.log(linkChart);
+
+		const ministeryCircleChart = buildMinistryComplexityCircleChart(this.circleSvgEle);
+		console.log(ministeryCircleChart);
+
+		const ministerySunburstChart = buildMinistryComplexitySunburstChart(this.sunburstSvgEle);
+		console.log(ministerySunburstChart);
+
 		return <>
 			<h1>Ministry Complexity Chart - NOT FINAL</h1>
 			<div class="row">
-				<svg className="img-fluid col-6" id={matrixChartId}></svg>
-				<svg className="img-fluid col-6" id={linkChartId}></svg>
+				<div className="img-fluid col-6">{this.matrixSvgEle}</div>
+				<div className="img-fluid col-6">{this.linkSvgEle}</div>
 			</div>
 			<div class="row">
-				<svg className="img-fluid col-6" id={ministryCircleChartId}></svg>
-				<svg className="img-fluid col-6" id={ministrySunburstChartId}></svg>
+				<div className="img-fluid col-6">{this.circleSvgEle}</div>
+				<div className="img-fluid col-6">{this.sunburstSvgEle}</div>
 			</div>
 		</>;
-	}
-
-	public rendered = (features) => {
-		const matrixSvgEle = document.getElementById(matrixChartId);
-		const matrixChart = buildInKindChart((matrixSvgEle as unknown) as SVGElement); // Yes, this really is an SVG element
-		console.log(matrixChart);
-
-		const linkSvgEle = document.getElementById(linkChartId);
-		const linkChart = buildProgramInteractionChart((linkSvgEle as unknown) as SVGElement);
-		console.log(linkChart);
-
-		const circleSvgEle = document.getElementById(ministryCircleChartId);
-		const ministeryCircleChart = buildMinistryComplexityCircleChart((circleSvgEle as unknown) as SVGElement); // Yes, this really is an SVG element
-		console.log(ministeryCircleChart);
-
-		const sunburstSvgEle = document.getElementById(ministrySunburstChartId);
-		const ministerySunburstChart = buildMinistryComplexitySunburstChart((sunburstSvgEle as unknown) as SVGElement); // Yes, this really is an SVG element
-		console.log(ministerySunburstChart);
 	}
 }
