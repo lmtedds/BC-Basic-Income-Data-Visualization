421017d138a4a83f1fdfafce93d446afdd874d3e
Peter Hunnisett <peter@softwarebalm.com>
2019-10-04 11:47:35 -0600

    Improve wrap text performance during transitions. - store length information in desc block so can, without full   recalculation, determine if we need to recalculate based on   width changing. - small performance improvements. - may make call more expensive on first call (i.e. if there are   no transitions) but it should be bearable.

--- src/charts/d3/text_wrap.ts
+++ src/charts/d3/text_wrap.ts
@@ -16,14 +16,64 @@ export interface IWrapTextDimenions {
 
 const canvas: HTMLCanvasElement = document.createElement("canvas");
 const context = canvas.getContext("2d");
-function offscreenGetWidth(text, fontSize?, fontFamily?) {
+
+function offscreenGetWidth(text): number {
 	// FIXME: Should also consider SVG's kerning, letter-spacing, and word-spacing attributes. Not sure how these translate.
-	if(fontSize && fontFamily) context.font = fontSize + " " + fontFamily;
 	return context.measureText(text).width;
 }
 
+function cacheOffscreenFont(fontSize: string, fontFamily: string): void {
+	context.font = fontSize + " " + fontFamily;
+}
+
+interface ILine {
+	text: string;
+	len: number;
+	max: number;
+}
+
+function buildLines(text: string, width: number): ILine[] {
+	const newWords = text.split(/(\s+)/);
+	const lines: ILine[] = [];
+	let newLine = "";
+
+	for(const word of newWords) {
+		const oldLine = newLine;
+		newLine += word;
+
+		const calculatedWidth = offscreenGetWidth(newLine);
+		if(calculatedWidth > width) {
+			const trimmed = oldLine.trim();
+			if(trimmed.length) {
+				lines.push({
+					text: trimmed,
+					len: offscreenGetWidth(trimmed),
+					max: calculatedWidth,
+				});
+			}
+
+			newLine = word;
+		}
+	}
+
+	if(newLine) {
+		const trimmed = newLine.trim();
+		if(trimmed.length) {
+			lines.push({
+				text: trimmed,
+				len: offscreenGetWidth(trimmed),
+				max: -1,
+			});
+		}
+	}
+
+	return lines;
+}
+
 // Wrap the words so that they fit inside the width provided.
-// NOTE: This approach has difficulty when working with transitions and updating or exiting.
+// NOTE: This approach has difficulty when working with transitions - this function will need to be called again
+//       with new width dimensions.
+// FIXME: We could work with size function (i.e. variable width based on height)
 export function wrapTextTspanEach(textEle: any, dimensions: IWrapTextDimenions): any {
 	const padding = dimensions.padding || 0;
 	const hCenter = dimensions.hCenter !== undefined ? dimensions.hCenter : false;
@@ -34,81 +84,69 @@ export function wrapTextTspanEach(textEle: any, dimensions: IWrapTextDimenions):
 	const x = textEle.attr("x");
 	const y = textEle.attr("y");
 
-	// Can dynamically get the element's style, but it's quite expensive when running
-	// this function as part of a d3 transition. For the time being remove it and
-	// require the caller to provide this information.
+	// Can dynamically get the element's style, but it's quite expensive. For the time being
+	// remove it and require the caller to provide this information.
 	// const textStyle = window.getComputedStyle(textEle.node());
 	// const fontSize = textStyle.fontSize || dimensions.fontSize;
 	// const fontFamily = textStyle.getPropertyValue("font-face") || dimensions.fontFamily;
 	// console.assert(dimensions.fontSize && dimensions.fontFamily, "font not provided");
 	const fontSize = dimensions.fontSize;
 	const fontFamily = dimensions.fontFamily;
+	cacheOffscreenFont(fontSize, fontFamily);
 
 	// See https://practicaltypography.com/line-spacing.html for suggestion of 120% to 145% line height.
 	const lineSpacing = 0.3; // 30% extra, so 130% line spacing.
 
-	// Stash the text in a <desc> block so that it can be used later as the definitive source.
-	const desc = textEle.select("desc");
-	const text = desc.empty() ? textEle.text() : desc.text();
-	const words = text.split(/\s+/).reverse();
-
-	// Create a desc block and populate it with the cannonical text. We can then retrieve it later
-	// if needed. Setting the text element's text to null will delete all the children (not sure why
-	// but it does on chrome for sure) so use it to get rid of either the text or the tspans that are
-	// there so we can start with a clean slate.
-	textEle.text(null);
-	textEle.append("desc").text(text);
-
 	// FIXME: There's a requirement that fontSize is in pixels.
 	let fontSizePx = 16; // try to work with a guess of 16px.
-	if(typeof fontSize === "string") {
-		const match = fontSize.match(/^([0-9.]+)px$/);
-		if(match) {
-			fontSizePx = Number(match[1]);
-		} else {
-			console.error(`unable to handle non pixel size: ${fontSize}`);
-		}
+	const match = fontSize.match(/^([0-9.]+)px$/);
+	if(match) {
+		fontSizePx = Number(match[1]);
+	} else {
+		console.error(`unable to handle non pixel size: ${fontSize}`);
 	}
 
 	// Font height in px is font size in pt (which is 0.75 px) => https://www.html5canvastutorials.com/tutorials/html5-canvas-text-metrics/
 	const lineHeight = fontSizePx * 0.75;
 
-	// Put all the words into lines. Simplistic approach to fit the most possible on each line.
-	// FIXME: What to do if the words don't fit in the space (including the height?) Probably just want 1 line stretched.
-	const lines = [];
-	let line = [];
-	while(words.length) {
-		const word = words.pop();
-		line.push(word);
+	// Stash the text in a <desc> block so that it can be used later as the definitive source.
+	let desc = textEle.select("desc");
+	const firstTextWrap = desc.empty();
+	let text: string;
+	let infoBlock: ILine;
 
-		const calculatedWidth = offscreenGetWidth(line.join(" "), fontSize, fontFamily);
-		if(calculatedWidth > width) {
-			if(line.length === 1) {
-				// FIXME: This word doesn't fit on a line... what do we want to do? Just plow on for now.
-				lines.push(line);
-			} else {
-				line.pop();
-				lines.push(line);
-				words.push(word);
-			}
-			line = [];
+	// Create a desc block and populate it with the cannonical text so that we can then retrieve it later
+	// if the same text element is resized.
+	if(firstTextWrap) {
+		text = textEle.text();
+		textEle.node().textContent = "";
+
+		infoBlock = {text: text, len: -1, max: -1};
+		desc = textEle.append("desc");
+	} else {
+		// FIXME: Need to see if we can shortcut recalc.
+		infoBlock = JSON.parse(desc.text());
+
+		// See if we have to rerun the text wrapping calculation. If we already fit
+		// in the dimensions, then a recalculation wouldn't do anything so don't bother.
+		if(width >= infoBlock.len && (infoBlock.max === -1 || width < infoBlock.max)) {
+			// console.log(`shortcutting for ${desc.text()} => ${width}`);
+			return;
 		}
+
+		text = infoBlock.text;
+
+		// FIXME: We could reuse tspans if we were clever
+		textEle.selectAll("tspan").remove();
 	}
 
-	// Catch the last line in case it fit.
-	if(line.length) lines.push(line);
-
-	// Calculate some dimensions
-	let longestLine = 0;
-	for(const thisLine of lines) {
-		const lineText = thisLine.join(" ");
-		const lineLength = offscreenGetWidth(lineText, fontSize, fontFamily);
-		if(longestLine <= lineLength) longestLine = lineLength;
-	}
+	const lines = buildLines(text, width);
 
 	for(let i = 0; i < lines.length; ++i) {
-		const lineText = lines[i].join(" ");
-		const lineLength = offscreenGetWidth(lineText, fontSize, fontFamily);
+		const lineText = lines[i].text;
+
+		infoBlock.len = Math.max(infoBlock.len, lines[i].len);
+		infoBlock.max = Math.max(infoBlock.max, lines[i].max);
 
 		let dx = 0;
 		let dy = (i + 1) * lineHeight + (i * lineHeight * lineSpacing); // line height + spacing between
@@ -116,8 +154,6 @@ export function wrapTextTspanEach(textEle: any, dimensions: IWrapTextDimenions):
 		// Alter dx as appropriate
 		if(hCenter) {
 			dx -= width / 2; // Wrap the words so that they fit inside the width provided.
-			// NOTE: This approach has difficulty when working with transitions and updating or exiting.
-
 		}
 
 		// Alter dy as appropriate
@@ -136,11 +172,11 @@ export function wrapTextTspanEach(textEle: any, dimensions: IWrapTextDimenions):
 				.attr("y", y || 0) // If y is specified for the text element we need to set y to the absolute position otherwise relative
 				.attr("dx", dx)
 				.attr("dy", dy)
-				.text(lines[i].join(" "));
-
+				.text(lineText);
 	}
 
-	// console.log(`text wrap "${textEle.text()}" => ${JSON.stringify(lines)}`);
+	// Write out the infoBlock.
+	desc.text(JSON.stringify(infoBlock));
 }
 
 // Wrap the words so that they fit inside the width provided.
