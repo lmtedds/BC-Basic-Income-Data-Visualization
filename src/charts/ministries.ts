7d0cc5381ce183efff10ee057197df7c74032ada
Gillian Petit <gillianschafer@gmail.com>
2020-02-18 14:02:00 -0700

    More colour stuff

--- src/charts/ministries.ts
+++ src/charts/ministries.ts
@@ -141,10 +141,8 @@ function eleToColour(key: string, level: number, parentColour: string): string {
 		return colour(key);
 	} else if(level === 2 ) {
 		return colour2(key);
-	} else if(level === 3) { 
-		function(d){
-			return d.colour3
-		};
+	} else if(level === 3) {
+		return colour3[key];
 	} else if(level === 4) {
 		return parentColour;
 	} else {
@@ -187,7 +185,7 @@ function treeToHierarchy(tree, obj: any = {ministry: "root", showName: false, va
 				showName: ele[showName] ? (ele[showName].toLowerCase() === "true") : false,
 				name: ele[programName] || ele[administeredBy] || ele[responsibleMinistry],
 				tooltip: makeTooltip(ele[fullProgramName], ele[description], ele[eligibility], ele[conditions], ele[expend201819], ele[recip201819], ele[cases2019], ele[expend201718], ele[child2018], ele[baseFund2018], ele[recip2017], ele[expend2017], ele[budget2019], ele[expend2019], ele[recip2019], ele[recip201718], ele[recip2018], ele[expend2018], ele[expend2016], ele[recip201617], ele[child] ),
-				colour: obj.colour,	
+				colour: obj.colour,
 			};
 		});
 	}
